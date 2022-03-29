dda2b858b30a85ea058ce6ede6edaaf06ceaddf8
yellowstonemagicstats <81812324+yellowstonemagicstats@users.noreply.github.com>
2022-03-29 16:18:26 -0500

    Update app.R
    test

--- app.R
+++ app.R
@@ -5,7 +5,7 @@
 #
 #    http://shiny.rstudio.com/
 #
-#wrong default charts, fix error message on lag, dark mode check
+#wrong default charts, dark mode check
 #DRUN, RA9pf, remove some pointless stuff from summary? 
 
 library(DT)
